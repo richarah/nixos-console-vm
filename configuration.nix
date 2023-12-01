eb7d61de7b14781a0847e1b8be7ed02ecae8107c
Richard Alexander Haydon <richarah@stud.ntnu.no>
2023-12-01 23:11:28 +0100

    Docker works!

--- configuration.nix
+++ configuration.nix
@@ -12,15 +12,37 @@
   # User configuration with automatic login
   users.users.alice = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" "docker" ]; # Enable ‘sudo’ for the user and add to Docker usergroup.
     packages = with pkgs; [
       tree
+      docker
     ];
     initialPassword = "testpw";
     createHome = true; # Automatically create home directory
     shell = "/run/current-system/sw/bin/bash"; # Specify the shell path
   };
 
+
+  networking.hostName = "docker-nixos";
+  i18n = {
+    consoleFont = "lat9w-16";
+    consoleKeyMap = "us";
+    defaultLocale = "en_US.UTF-8";
+  };
+
+  services = {
+    openssh = {
+      enable = true;
+    };
+  };
+  # Rootless Docker, VM issues
+  virtualisation.docker = {
+    enable = true;
+    rootless = {
+      enable = true;
+      setSocketVariable = true;
+    };
+  };
   # Enable automatic login for the specified user using getty
   services.getty.autologinUser = "alice";
   system.stateVersion = "23.11";
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Ugly, but there doesn't appear to be a way to do this in config.
-export QEMU_OPTS="-nographic -serial mon:stdio" QEMU_KERNEL_PARAMS=console=ttyS0 ; ./result/bin/run-nixos-vm
+export QEMU_OPTS="-nographic -serial mon:stdio" QEMU_KERNEL_PARAMS=console=ttyS0 ; ./result/bin/run-docker-nixos-vm
 
