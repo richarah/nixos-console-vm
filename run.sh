310f376cde79e6f9fff37300da2d80bc6f1d0e23
Richard Alexander Haydon <richarah@eirin.pvv.ntnu.no>
2023-12-03 18:28:26 +0100

    Another merge messup?

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # TODO: move to config
-export QEMU_OPTS="-nographic -serial mon:stdio" QEMU_KERNEL_PARAMS=console=ttyS0 ; ./result/bin/run-docker-nixos-vm
+export QEMU_OPTS="-nographic -serial mon:stdio -m 8G -smp $(nproc)" QEMU_KERNEL_PARAMS=console=ttyS0 ; ./result/bin/run-nixos-vm
